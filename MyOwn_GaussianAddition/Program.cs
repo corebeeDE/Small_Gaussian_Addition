0e7e0d208c9eb45c953937afc406c0aa0d249d5e
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-05-18 22:33:45 +0200

    Projektdateien hinzufügen.

---
+++ MyOwn_GaussianAddition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwn_GaussianAddition
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetAddition(0, 100));
+            Console.ReadKey();
+        }
+
+        // This is the "small Gauss", only param "end" can be set, the rest needs to start from 1
+        public static double GetAddition(int start, int end)
+        {
+            double sum = 0;
+            while (start < end)
+            {
+                sum += ++start;
+            }
+            return sum;
+        }
+    }
+}
